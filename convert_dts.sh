405fa7deb4e51f3719b62a976ee1712e4771771e
Ali Tariq <alitariq4589@gmail.com>
2025-03-24 12:58:03 +0500

    Added dts conversion error log

--- convert_dts.sh
+++ convert_dts.sh
@@ -1,1 +1,3 @@
-dtc -O dtb -o /nfsroot/bootfs/k1-x_deb1_cloud_v.dtb k1-x_deb1_cloud_v.dts
+#!/bin/bash
+
+dtc -O dtb -o /nfsroot/bootfs/k1-x_deb1_cloud_v.dtb k1-x_deb1_cloud_v.dts > >(tee dts_conversion.log) 2>&1
